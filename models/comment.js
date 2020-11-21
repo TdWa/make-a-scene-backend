27eaebf5f8ffbf2d089ebcdfc50d6311ed0c1942
TdWa <59452572+TdWa@users.noreply.github.com>
2020-11-21 15:17:08 +0100

    fix comment relations

--- models/comment.js
+++ models/comment.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       comment.belongsTo(models.scene);
+      comment.belongsTo(models.user);
     }
   }
   comment.init(
