463fdbd4827c227ba09d661b5479e88ea7d6c835
TdWa <59452572+TdWa@users.noreply.github.com>
2020-11-24 11:40:20 +0100

    add signup/login endpoints

--- index.js
+++ index.js
@@ -7,23 +7,17 @@ app.set("json spaces", 2);
 // const productRouter = require("./routers/productRouter");
 // const userRouter = require("./routers/userRouter");
 // const orderRouter = require("./routers/orderRouter");
-// const loginRouter = require("./routers/loginRouter");
-// const authMiddleware = require("./auth/middleware");
-// const meRouter = require("./routers/meRouter");
+const authRouter = require("./routers/authRouter");
 
 app.use(cors());
 app.use(express.json());
-// app.use("/login", loginRouter);
-// app.use("/me", authMiddleware, meRouter);
+app.use("/", authRouter);
 // app.use("/orders", authMiddleware, orderRouter);
 // app.use("/products", productRouter);
 // app.use("/users", userRouter);
 
-app.get("/", async (req, res, next) => {
+app.get("/test", (req, res) => {
   res.json("hello world");
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-// test development branch
-// test login-system branch
