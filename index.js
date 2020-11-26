9f11134608b46a3fa62ead3de7425ca0c37ae3c2
TdWa <59452572+TdWa@users.noreply.github.com>
2020-11-26 12:35:39 +0100

    add users endpoint to edit about text

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const { PORT } = require("./config/constants");
 app.set("json spaces", 2);
 
 // const productRouter = require("./routers/productRouter");
-// const userRouter = require("./routers/userRouter");
 // const orderRouter = require("./routers/orderRouter");
+const userRouter = require("./routers/userRouter");
 const authRouter = require("./routers/authRouter");
 
 app.use(cors());
 app.use(express.json());
 app.use("/", authRouter);
+app.use("/users", userRouter);
 // app.use("/orders", authMiddleware, orderRouter);
 // app.use("/products", productRouter);
-// app.use("/users", userRouter);
 
 app.get("/test", (req, res) => {
   res.json("hello world");
