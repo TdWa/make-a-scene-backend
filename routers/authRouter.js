665e0c33b82a3c718e5ac4dce26ac8b7f2c4f5dd
TdWa <59452572+TdWa@users.noreply.github.com>
2020-11-27 21:48:34 +0100

    include scenes exclude created/updatedAt

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -2,42 +2,10 @@ const { Router } = require("express");
 const { toJWT } = require("../auth/jwt");
 const bcrypt = require("bcrypt");
 const authMiddleware = require("../auth/middleware");
-const User = require("../models").user;
+const { user: User, scene: Scene, actor: Actor } = require("../models");
 const { SALT_ROUNDS } = require("../config/constants");
 
 const router = new Router();
-
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Please provide both email and password" });
-    }
-
-    const user = await User.findOne({ where: { email } });
-
-    if (!user) {
-      return res.status(404).json({
-        message: "User with that email not found",
-      });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return res.status(401).json({
-        message: "Incorrect password",
-      });
-    }
-
-    delete user.dataValues["password"]; // don't send back the password hash
-    const token = toJWT({ userId: user.id });
-    return res.status(200).json({ token, ...user.dataValues });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ message: "Something went wrong, sorry" });
-  }
-});
 
 router.post("/signup", async (req, res) => {
   const { email, password, name } = req.body;
@@ -73,9 +41,53 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-// The /me endpoint can be used to:
-// - get the users email & name using only their token
-// - checking if a token is (still) valid
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please provide both email and password" });
+    }
+
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: Scene,
+          attributes: ["id", "name"],
+          include: [
+            {
+              model: Actor,
+              attributes: ["id", "type", "name", "backgroundColor", "color"],
+            },
+          ],
+        },
+      ],
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User with that email not found",
+      });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({
+        message: "Incorrect password",
+      });
+    }
+
+    delete user.dataValues["password"]; // don't send back the password hash
+    const token = toJWT({ userId: user.id });
+    return res.status(200).json({ token, ...user.dataValues });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: "Something went wrong, sorry" });
+  }
+});
+
 router.get("/me", authMiddleware, async (req, res) => {
   // don't send back the password hash
   delete req.user.dataValues["password"];
